0c7d0f7255caca7e4c184e546087560b568e4f8d
张晨 <zhangchen@jcmsc.cn>
2022-11-07 16:27:30 +0800

    init commit

--- featureSample.py
+++ featureSample.py
@@ -42,37 +42,54 @@ def get_sample(data, res_x, res_y, num):
     return feature
 def get_data():
     pass
-file_name = [
-            'data/Lac-DPE-6SL---Cel-DPE-6SL---Mal-DPE-6SL/Cel-DPE-6SL-28930 events',
-             'data/Lac-DPE-6SL---Cel-DPE-6SL---Mal-DPE-6SL/Lac-DPE-6SL-27696 events',
-             'data/Lac-DPE-6SL---Cel-DPE-6SL---Mal-DPE-6SL/Mal-DPE-6SL-31678 events'
-             ]
 
-mol_num = len(file_name)
-data = [pd.read_excel(i+'.xlsx') for i in file_name]
-t = pd.read_excel('data/Lac-DPE-6SL---Cel-DPE-6SL---Mal-DPE-6SL/Mal-DPE-6SL-31678 events.xlsx')
-x = [i['value1'].values for i in data]
-y = [i['value2'].values for i in data]
-tmp_x = [m for n in x for m in n]
-tmp_y = [m for n in y for m in n]
 
-x_tmp = sorted(tmp_x)
-y_tmp = sorted(tmp_y)
-num = 3  # 等分个数
-res_x, res_y = split_slice_n(x_tmp, y_tmp, num)
-print(res_x)
-print(res_y)
 
-for j in range(mol_num):
+def get_feature(file_name, sample_len, num):
+    """
+
+    :param file_name:  需要处理的文件名列表
+    :param sample_len: 短读数据长度
+    :param num: 几等分
+    :return: 在当前目录生成特征文件
+    """
+    mol_num = len(file_name)
+    data = [pd.read_excel(i+'.xlsx') for i in file_name]
+    t = pd.read_excel('data/Lac-DPE-6SL---Cel-DPE-6SL---Mal-DPE-6SL/Mal-DPE-6SL-31678 events.xlsx')
+    x = [i['value1'].values for i in data]
+    y = [i['value2'].values for i in data]
+    tmp_x = [m for n in x for m in n]
+    tmp_y = [m for n in y for m in n]
+
+    x_tmp = sorted(tmp_x)
+    y_tmp = sorted(tmp_y)
+
+    res_x, res_y = split_slice_n(x_tmp, y_tmp, num)
+    # print(res_x)
+    # print(res_y)
+
+    for j in range(mol_num):
+
+        start = 0
+        temp_len = data[j].shape[0]
+        features = []
+        for i in range(temp_len//sample_len):
+            data_value = data[j][start:sample_len*(i+1)].values
+            feature = get_sample(data_value, res_x, res_y, num)
+            feature /= sample_len
+            start = sample_len*(i+1)
+            features.append(feature)
+        t = pd.DataFrame(features)
+        t.to_csv(file_name[j]+'By'+str(num)+'on'+str(sample_len)+'mol3.csv')
+
+
+if __name__=='__main__':
+    file_name = [
+        'data/Lac-DPE-6SL---Cel-DPE-6SL---Mal-DPE-6SL/Cel-DPE-6SL-28930 events',
+        'data/Lac-DPE-6SL---Cel-DPE-6SL---Mal-DPE-6SL/Lac-DPE-6SL-27696 events',
+        'data/Lac-DPE-6SL---Cel-DPE-6SL---Mal-DPE-6SL/Mal-DPE-6SL-31678 events'
+    ]
+    num = 3  # 等分个数
     sample_len = 2000
-    start = 0
-    temp_len = data[j].shape[0]
-    features = []
-    for i in range(temp_len//sample_len):
-        data_value = data[j][start:sample_len*(i+1)].values
-        feature = get_sample(data_value, res_x, res_y, num)
-        feature /= sample_len
-        start = sample_len*(i+1)
-        features.append(feature)
-    t = pd.DataFrame(features)
-    t.to_csv(file_name[j]+'By'+str(num)+'on'+str(sample_len)+'mol3.csv')
+
+    get_feature(file_name, sample_len, num)
