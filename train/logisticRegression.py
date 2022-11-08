fc122c6ee3307549062ca2c923c7581c21938f4b
张晨 <zhangchen@jcmsc.cn>
2022-11-08 16:38:15 +0800

    添加mol_num到confusion_matrix

--- train/logisticRegression.py
+++ train/logisticRegression.py
@@ -107,24 +107,18 @@ if __name__ == '__main__':
             print('predicting cost %f seconds' % (time_4 - time_3))
 
             # 采用混淆矩阵（metrics）计算各种评价指标
-
             ps.append(metrics.precision_score(test_labels, test_predict, average='weighted'))
-            # rs.append(metrics.recall_score(test_labels, test_predict,labels=[0,1,2], average='weighted'))
+            rs.append(metrics.recall_score(test_labels, test_predict, labels=[0, 1, 2], average='weighted'))
             fs.append(metrics.f1_score(test_labels, test_predict, average='weighted'))
             cs.append(np.mean(test_labels == test_predict))
 
             # 分类报告 看有几类分子
             class_report = metrics.classification_report(test_labels, test_predict,
-                                                         target_names=["Cel-DPE6SL",
-                                                                       "Lac-DPE6SL", "Mal-DPE6SL"])
+                                                         target_names=["Cel-DPE6SL", "Lac-DPE6SL", "Mal-DPE6SL"])
             print(class_report)
             # 输出混淆矩阵
             # confusion_matrix = metrics.confusion_matrix(test_labels, test_predict)
-            cm = metrics.confusion_matrix(test_labels, test_predict)
-            confusion_matrix += cm
-            rs.append(calculate_recall(cm))
-            ps.append(calculate_prediction(cm))
-
+            confusion_matrix += metrics.confusion_matrix(test_labels, test_predict)
 
         print('--混淆矩阵--')
         print(confusion_matrix)
