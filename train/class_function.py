2fdba7b897e80cf731dce02d6e8ecc99a9e489dd
张晨 <zhangchen@jcmsc.cn>
2023-06-06 09:11:50 +0800

    分类算法实现可批量处理文件

--- train/class_function.py
+++ train/class_function.py
@@ -2,14 +2,12 @@
 # author:Lucifer_Chen
 # contact: zhnagchen@17888808985.com
 # datetime:2023/5/12 14:27
-
 """
-文件说明：  
+文件说明：
 """
 import os
 import time
 from datetime import datetime
-
 import joblib
 import numpy as np
 import openpyxl
@@ -23,6 +21,7 @@ from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 
+
 def polt_conf(cm, classes, name):
     """
 
@@ -31,7 +30,6 @@ def polt_conf(cm, classes, name):
     :param name: 保存图像名
     :return:
     """
-
     # 绘制混淆矩阵图像
     fig, ax = plt.subplots()
     im = ax.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
@@ -48,8 +46,12 @@ def polt_conf(cm, classes, name):
     plt.show()
 
 
-
 def load_forest_data(file_name):
+    """
+    加载样本数据，分层抽样
+    :param file_name:
+    :return:
+    """
     features = []
     labels = []
     for index, file in enumerate(file_name):
@@ -65,29 +67,17 @@ def load_forest_data(file_name):
     return train_features, test_features, train_labels, test_labels
 
 
-if __name__ == '__main__':
+def train(file_name, model_type, sample_type, nmr_name):
     # 获取当前日期,为后面模型添加时间戳
     now = datetime.now()
     date = now.strftime("%Y-%m-%d")
-    epoch = 100
-    print('prepare datasets...')
-    # LogisticRegression, random_forest, support_vector_machine, AdaBoost, naive_bayes
-    condition = 'LogisticRegression'  # 算法开关
-    sample_type = 'windows'
-    # 列出指定目录下的文件夹路径
-    path = '../data/T240R二糖-NNR-Windows/featureBy3on2000' + sample_type
-    file_name = [os.path.join(path, f) for f in os.listdir(path)]
-    nmr_name = [f.strip('-feature.csv') for f in os.listdir(path)]  # 使用的数据名称
-    mol_num = len(file_name)
-
     # 结果保存
-    save_path = '../' + sample_type + date + 'result.xlsx'
+    save_path = '../result/' + sample_type + date + 'result.xlsx'
     if not os.path.exists(save_path):
         df = pd.DataFrame()  # 表示创建空的表
         df.to_excel(save_path)
-
+    mol_num = len(file_name)
     time_2 = time.time()
-    model_path = None
     test_acc = []
     train_acc = []
     all_confusion_matrix = []
@@ -95,32 +85,31 @@ if __name__ == '__main__':
     confusion_matrix.tolist()
     # 各项指标
     all_ps, all_rs, all_fs, all_cs, all_roc = [], [], [], [], []
-
     # 根据条件选择要使用的机器学习算法
-
-    if condition == "naive_bayes":
+    if model_type == "naive_bayes":
         model = MultinomialNB(alpha=1.0)  # 加入laplace平滑
 
-    elif condition == "AdaBoost":
+    elif model_type == "AdaBoost":
         model = AdaBoostClassifier(n_estimators=100, algorithm='SAMME.R')
-    elif condition == "support_vector_machine":
+    elif model_type == "support_vector_machine":
         """
            C：SVC的惩罚参数，默认值是1.0；C越大，对误分类的惩罚增大，趋向于对训练集完全分对的情况，这样对训练集测试时准确率很高，但泛化能力弱。
            C值小，对误分类的惩罚减小，允许容错，将他们当成噪声点，泛化能力较强。
            kernel ：核函数，默认是rbf，可以是‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’
         """
         model = svm.SVC(C=1.0, kernel='rbf')  # poly效果最好
-    elif condition == "random_forest":
+    elif model_type == "random_forest":
         model = RandomForestClassifier(n_estimators=80)  # 容易区分则设置为25
-    elif condition == "LogisticRegression":
+    elif model_type == "LogisticRegression":
         model = linear_model.LogisticRegression()
     else:
         raise ValueError("Invalid condition specified")
-
+    epoch = 100
 
     for k in range(10):
         ps, rs, fs, cs, roc = [], [], [], [], []
         for i in range(epoch):
+            print('prepare datasets...(every time)')
             # 自己数据集加载
             train_features, test_features, train_labels, test_labels = load_forest_data(file_name)
             print('Start training...')
@@ -130,7 +119,7 @@ if __name__ == '__main__':
             train_acc.append(train_score)
             print("训练集：", train_score)
 
-            joblib.dump(model, "../models/"+condition+date+sample_type+'.m')
+            joblib.dump(model, "../models/" + model_type + date + sample_type + '.m')
             time_3 = time.time()
             print('training cost %f seconds' % (time_3 - time_2))
 
@@ -187,27 +176,25 @@ if __name__ == '__main__':
     print(cs_std)
     print('混淆矩阵*')
     print(confusion_matrix)
-    polt_conf(confusion_matrix, nmr_name, "../picture/" + condition + date +sample_type +'.png')
+    polt_conf(confusion_matrix, nmr_name, "../picture/" + model_type + date + sample_type + '.png')
     data = {'Acc': str(round(cs_mean, 4)) + '±' + str(round(cs_std, 4)),
             'precision': str(round(ps_mean, 4)) + '±' + str(round(ps_std, 4)),
             'recall': str(round(rs_mean, 4)) + '±' + str(round(rs_std, 4)),
             'F1': str(round(fs_mean, 4)) + '±' + str(round(fs_std, 4))}
 
-
     with pd.ExcelWriter(save_path, mode='a', engine='openpyxl') as writer:
         df = pd.DataFrame(data, index=[0])
 
-        df.to_excel(writer, sheet_name=condition, index=False)
+        df.to_excel(writer, sheet_name=model_type, index=False)
         df1 = pd.DataFrame(confusion_matrix)
-        df1.to_excel(writer, sheet_name=condition+'_confusion_matrix', header=False, index=False)
+        df1.to_excel(writer, sheet_name=model_type + '_confusion_matrix', header=False, index=False)
 
-   # 删除空表头Sheet1
+    # 删除空表头Sheet1
     # 执行删除操作：
     sheet_name = 'Sheet1'
     # 载入工作簿
     try:
         workbook = openpyxl.load_workbook(save_path)
-        #
         # 删除目标Sheet
         worksheet = workbook[sheet_name]
         workbook.remove(worksheet)
@@ -216,3 +203,26 @@ if __name__ == '__main__':
         workbook.save(save_path)
     except:
         print('No sheet1')
+
+
+if __name__ == '__main__':
+    # conditions = ['LogisticRegression', 'random_forest', 'support_vector_machine', 'AdaBoost', 'naive_bayes']
+    # sampleTypes = ['windows', 'base']
+    conditions, sampleTypes = False, False
+    if conditions and sampleTypes:
+        for sampleType in sampleTypes:
+            for condition in conditions:
+                # 列出指定目录下的文件夹路径
+                path = '../data/T240R二糖-NNR/featureBy3on2000' + sampleType
+                fileName = [os.path.join(path, f) for f in os.listdir(path)]
+                nmrName = [f.strip('-feature.csv') for f in os.listdir(path)]  # 使用的数据名称
+                train(file_name=fileName, model_type=condition, sample_type=sampleType, nmr_name=nmrName)
+    else:
+        condition = 'random_forest'  # 算法开关
+        sampleType = 'random'
+        # 列出指定目录下的文件夹路径
+        path = '../data/T240R二糖-NNR/featureBy3on2000' + sampleType
+        fileName = [os.path.join(path, f) for f in os.listdir(path)]
+        nmrName = [f.strip('-feature.csv') for f in os.listdir(path)]  # 使用的数据名称
+        train(file_name=fileName, model_type=condition, sample_type=sampleType, nmr_name=nmrName)
+
